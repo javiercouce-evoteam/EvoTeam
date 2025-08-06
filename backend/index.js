d85d323317e4b170ba142a60886770f7a10da04f
jcouce <jcoucelanga@gmail.com>
2025-08-06 13:53:22 +0200

    refactor(backend): remove obsolete JavaScript entry file

--- backend/index.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (_, res) => res.send('Hello from backend'));
-app.listen(3001, () => console.log('API running on http://localhost:3001'));
-
